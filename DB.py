dc28cf7122b17bc3089c199d8d9f07b250f9123a
Bharath Kotha <bharath.charan.reddy@gmail.com>
2021-10-14 15:48:46 +0530

    Connect to database using env variables

---
+++ DB.py
@@ -0,0 +1,29 @@
+import os
+
+import psycopg2
+from psycopg2 import OperationalError
+
+db_name = os.getenv("DB_NAME", "postgres")
+db_user = os.getenv("DB_USER", "postgres")
+db_password = os.getenv("DB_PASSWORD", "postgres")
+db_host = os.getenv("DB_HOST", "localhost")
+db_port = os.getenv("DB_PORT", 5432)
+
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+connection = create_connection(db_name, db_user, db_password, db_host, db_port)
---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions on running
+* Add the following environment variables
+  - DB_NAME
+  - DB_USER
+  - DB_PASSWORD
+  - DB_HOST
+  - DB_PORT
