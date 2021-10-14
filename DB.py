b0dddf7aa37cbf4032968ba15a2d3d4d2afaf3db
Bharath Kotha <bharath.charan.reddy@gmail.com>
2021-10-14 18:28:12 +0530

    Run basic commands in the database

--- DB.py
+++ DB.py
@@ -27,3 +27,30 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
 
 
 connection = create_connection(db_name, db_user, db_password, db_host, db_port)
+
+# Trying creating a table, insert row and read
+cur = connection.cursor()
+
+CREATE_TABLE_QUERY = """ CREATE TABLE IF NOT EXISTS test_table(
+        id serial PRIMARY KEY,
+        value INTEGER
+    )
+"""
+
+cur.execute(CREATE_TABLE_QUERY)
+
+INSERT_QUERY = """ INSERT INTO test_table (value) VALUES (1) """
+
+cur.execute(INSERT_QUERY)
+
+SELECT_QUERY = """ SELECT * FROM test_table; """
+
+cur.execute(SELECT_QUERY)
+print(cur.fetchone())
+
+DELETE_QUERY = """ DROP TABLE test_table """
+cur.execute(DELETE_QUERY)
+
+connection.commit()
+cur.close()
+connection.close()
